57bbfed994bc0b7c991278d5539ac11380d93e8e
ahmed karam <ahmeadkaram2020@gmail.com>
2020-05-04 02:09:07 +0200

    Adding About Us Section

--- js/code.js
+++ js/code.js
@@ -9,14 +9,26 @@ window.onscroll = function () {
     } else{
         nav.classList.remove('nav-fixed');
         logo.setAttribute('src', 'images/logo.png');
-    }   
+    }
+    scrollTopCheacker();
 }
 
-// Animation
+// Start Animation
 window.onload = function () {
     let smartPhone = document.getElementById("smart-phone");
     console.log(smartPhone);
     smartPhone.style.animationPlayState = 'running';
+    scrollTopCheacker();
 }
+//End Animation
 
-console.log('ahmed');
+function scrollTopCheacker() {
+    var a = $('.about-us').offset().top,
+        b = $(window).scrollTop();
+
+    if((b + 100) >= a){
+        $('.card-box').css({
+            animationPlayState : 'running'
+        })
+    }
+}
